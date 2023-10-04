3af9815e3cf9c8e5bd30009535a0c95477f02115
JonahMorgan-2002 <jonah.morgan@selu.edu>
2023-10-04 15:05:59 -0500

    Commit

---
+++ GeoObjectTest.java
@@ -0,0 +1,16 @@
+package chapter13;
+
+import java.awt.*;
+
+public class GeoObjectTest {
+    public static void main(String []args) {
+        Circle circle = new Circle();
+        circle.printCircle();
+        GeometricObject geoObj;
+        geoObj = new Circle();
+        //GeometricObject geoObj2 = new GeometricObject();
+        Rectangle rectangle = new Rectangle();
+        GeometricObject geoObj3 = new Rectangle();
+
+    }
+}
